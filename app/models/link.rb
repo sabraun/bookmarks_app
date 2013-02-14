53b81ec99c74f81150a0e8e34620bd231efd75f1
Steve Braun <sabraun@madisoncollege.edu>
2013-02-14 14:28:40 -0600

    url validation8

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,7 @@ class Link < ActiveRecord::Base
   attr_accessible :URL, :dateSaved, :name
 
   validates :name, :length => {:maximum => 8}
+  validates :name, :length => {:minimum => 2}
   validates :URL, :length => {:minimum => 3}
 
 
