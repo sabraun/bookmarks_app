aa0f971b96cd90653201f6b1d1e8986447554c6d
Steve Braun <sabraun@madisoncollege.edu>
2013-02-23 15:38:56 -0600

    final

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,6 @@ class Link < ActiveRecord::Base
   attr_accessible :URL, :dateSaved, :name
 
   validates :name, :length => {:maximum => 20, :minimum => 2}
-  validates :URL, :length => {:minimum => 3}
-
+  validates :URL, :length => {:minimum => 4}
 
 end
--- spec/requests/links_spec.rb
+++ spec/requests/links_spec.rb
@@ -2,23 +2,16 @@ require 'spec_helper'
 
 describe "Links" do
     
-    #it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-    #  get links_path
-    #  response.status.should be(200)
-    #end
-    
-    it "should have the h1 'links/new'" do
+    it "should have the <h1> 'links/new'" do
         visit '/links/new'
         page.should have_selector('h1', :text => 'New link')
     end        # test ok
     
-    it "should have the h1 'links_form'" do
+    
+    it "should have the <h1> 'links_form'" do
         visit '/links'
         page.should have_selector('h1', :text => 'Listing links')
     end        # test ok
-    
-
     
 end
 
