747e8e78855156031295477483537dd1d69f540b
Steve Braun <sabraun@madisoncollege.edu>
2013-04-10 16:34:03 -0500

    end ch6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com", 
+      @user = User.new(name: "Example User", email: "user@example.com", realName: "John Smith",
                      password: "foobarx", password_confirmation: "foobarx")
   end
 
@@ -17,8 +17,9 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
 
-  # failing test ???
-  it { should be_valid }
+
+    it { should be_valid }
+
    
   describe "when name is not present" do
     before { @user.name = " " }
