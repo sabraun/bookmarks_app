f1a28b2ad4698ba11c8d1f98d804e61f2300d33f
Steve Braun <sabraun@madisoncollege.edu>
2013-04-14 22:13:25 -0500

    912

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
-    
+  before_filter :signed_in_user, only: [:edit, :update]
+   
   def new
     @user = User.new
   end
-  
+   
   def show
     @user = User.find(params[:id])
   end
-  
+   
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -19,4 +20,29 @@ class UsersController < ApplicationController
     end
   end
 
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  
+  private
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+  
+  
 end
